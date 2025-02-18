8b2a9d8b92774e735dc0934b6e2b288b6b427130
Ezequiel Purro <ezequiel.purro@gmail.com>
2025-02-18 15:14:34 -0300

    about me started

---
+++ src/data/techData.js
@@ -0,0 +1,18 @@
+
+export const techData = [
+    { name: 'logos/javascript', class: 'javascript text-[6rem]', category: 'Programming Languages' },
+    { name: 'logos/typescript', class: 'typescript text-[6rem]', category: 'Programming Languages' },
+    { name: 'logos/python', class: 'python text-[6rem]', category: 'Programming Languages' },
+    { name: 'logos/java', class: 'java text-[6rem]', category: 'Programming Languages' },
+    
+    { name: 'logos/html', class: 'html text-[6rem]', category: 'Frontend Development' },
+    { name: 'logos/css', class: 'css text-[6rem]', category: 'Frontend Development' },
+    { name: 'logos/react', class: 'react text-[6rem]', category: 'Frontend Development' },
+    { name: 'logos/tailwind', class: 'tailwind text-[6rem]', category: 'Frontend Development' },
+
+    { name: 'logos/django', class: 'django text-[6rem]', category: 'Backend Development' },
+    { name: 'logos/node', class: 'node text-[6rem]', category: 'Backend Development' },
+    { name: 'logos/express', class: 'express text-[6rem]', category: 'Backend Development' },
+
+    { name: 'logos/mongodb', class: 'mongodb text-[6rem]', category: 'Databases' },
+];
