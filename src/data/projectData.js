bcff5deaec55b465b2cb0c28982ebf40f364c7f7
Ezequiel Purro <ezequiel.purro@gmail.com>
2025-02-21 14:37:41 -0300

    Techs added to each project

--- src/data/projectData.js
+++ src/data/projectData.js
@@ -6,7 +6,12 @@ export const projectData = [
         shortDescription: 'Designed a scheduler to optimize appointment reservations for a local beauty center, enhancing efficiency and customer experience.',
         link: '',
         githubRepo: 'https://github.com/ezepurro/turnero-NICoworking',
-        description: ''
+        techs: [
+            {name: 'logos/react'},
+            {name: 'logos/javascript'},
+            {name: 'logos/mongodb',},
+            {name: 'logos/express'},
+        ]
     },
     {
         title: 'Web Planner',
@@ -14,6 +19,10 @@ export const projectData = [
         shortDescription: 'Web planner built with Django for organizing and managing household tasks efficiently.',
         link: '',
         githubRepo: 'https://github.com/ezepurro/planificadorweb-django',
-        description: ''
+        techs: [
+            {name: 'logos/django'},
+            {name: 'logos/python'},
+            {name: 'logos/bootstrap'},
+        ]
     },
 ];
--- src/data/techData.js
+++ src/data/techData.js
@@ -1,19 +1,19 @@
 
 export const techData = [
-    { name: 'logos/javascript', class: 'javascript text-[6rem]', category: 'Programming Languages' },
-    { name: 'logos/typescript', class: 'typescript text-[6rem]', category: 'Programming Languages' },
-    { name: 'logos/python', class: 'python text-[6rem]', category: 'Programming Languages' },
-    { name: 'logos/java', class: 'java text-[6rem]', category: 'Programming Languages' },
+    { name: 'logos/javascript', class: 'javascript text-[6rem] text-[#F7DF1E] md:text-white', category: 'Programming Languages' },
+    { name: 'logos/typescript', class: 'typescript text-[6rem] text-[#3178C6] md:text-white', category: 'Programming Languages' },
+    { name: 'logos/python', class: 'python text-[6rem] text-[#3776AB] md:text-white', category: 'Programming Languages' },
+    { name: 'logos/java', class: 'java text-[6rem] text-[#EC2025] md:text-white', category: 'Programming Languages' },
     
-    { name: 'logos/html', class: 'html text-[6rem]', category: 'Frontend Development' },
-    { name: 'logos/css', class: 'css text-[6rem]', category: 'Frontend Development' },
-    { name: 'logos/react', class: 'react text-[6rem]', category: 'Frontend Development' },
-    { name: 'logos/astro', class: 'astro text-[6rem]', category: 'Frontend Development' },
-    { name: 'logos/tailwind', class: 'tailwind text-[6rem]', category: 'Frontend Development' },
+    { name: 'logos/html', class: 'html text-[6rem] text-[#E34F26] md:text-white', category: 'Frontend Development' },
+    { name: 'logos/css', class: 'css text-[6rem] text-[#1572B6] md:text-white', category: 'Frontend Development' },
+    { name: 'logos/react', class: 'react text-[6rem] text-[#61DAFB] md:text-white', category: 'Frontend Development' },
+    { name: 'logos/astro', class: 'astro text-[6rem] text-[#FF5D01] md:text-white', category: 'Frontend Development' },
+    { name: 'logos/tailwind', class: 'tailwind text-[6rem] text-[#38B2AC] md:text-white', category: 'Frontend Development' },
 
-    { name: 'logos/django', class: 'django text-[6rem]', category: 'Backend Development' },
-    { name: 'logos/node', class: 'node text-[6rem]', category: 'Backend Development' },
-    { name: 'logos/express', class: 'express text-[6rem]', category: 'Backend Development' },
+    { name: 'logos/django', class: 'django text-[6rem] text-[#092E20] md:text-white', category: 'Backend Development' },
+    { name: 'logos/node', class: 'node text-[6rem] text-[#68A063] md:text-white', category: 'Backend Development' },
+    { name: 'logos/express', class: 'express text-[6rem] text-white', category: 'Backend Development' },
 
-    { name: 'logos/mongodb', class: 'mongodb text-[6rem]', category: 'Databases' },
+    { name: 'logos/mongodb', class: 'mongodb text-[6rem] text-[#47A248] md:text-white', category: 'Databases' },
 ];
