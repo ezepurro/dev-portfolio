f26a8fca31fb54a1a0c2ee432de24562fcb82131
Ezequiel Purro <ezequiel.purro@gmail.com>
2025-03-13 12:32:07 -0300

    animations package added

--- src/data/socialMediaData.js
+++ src/data/socialMediaData.js
@@ -1,7 +1,7 @@
 
 
 export const socialMediaData = [
-    {name: 'LinkedIn', link: 'https://www.linkedin.com/in/ezequiel-purro-75920b7a/'},
+    {name: 'LinkedIn', link: 'https://www.linkedin.com/in/ezequiel-purro/'},
     {name: 'GitHub', link: 'https://github.com/ezepurro'},
     {name: 'Instagram', link: 'https://www.instagram.com/ezepurro/'},
 ]
