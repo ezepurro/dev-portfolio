6c36430ab78ff85a1afd8d35c659458439471e07
Ezequiel Purro <ezequiel.purro@gmail.com>
2026-08-05 15:14:01 -0300

    portfolio updated

--- src/data/projectData.js
+++ src/data/projectData.js
@@ -1,6 +1,19 @@
 
 export const projectData = [
     {
+        title: 'Web Planner',
+        img: 'img/planificador-web-1.png',
+        shortDescription: 'Web planner for organizing and managing household tasks efficiently.',
+        link: '',
+        githubRepo: 'https://github.com/ezepurro/planificadorweb-django',
+        techs: [
+            {name: 'logos/django'},
+            {name: 'logos/python'},
+            {name: 'logos/bootstrap'},
+        ],
+        releaseDate: 'AUG 2024'
+    },
+    {
         title: 'Beauty Center',
         img: 'img/beauty-center-1.png',
         shortDescription: 'Designed a scheduler to optimize appointment reservations for a local beauty center, enhancing efficiency and customer experience.',
@@ -11,19 +24,8 @@ export const projectData = [
             {name: 'logos/javascript'},
             {name: 'logos/mongodb',},
             {name: 'logos/express'},
-        ]
-    },
-    {
-        title: 'Web Planner',
-        img: 'img/planificador-web-1.png',
-        shortDescription: 'Web planner for organizing and managing household tasks efficiently.',
-        link: '',
-        githubRepo: 'https://github.com/ezepurro/planificadorweb-django',
-        techs: [
-            {name: 'logos/django'},
-            {name: 'logos/python'},
-            {name: 'logos/bootstrap'},
-        ]
+        ],
+        releaseDate: 'MAY 2025'
     },
     {
         title: 'Solid Cat Landing Page',
@@ -35,6 +37,22 @@ export const projectData = [
             {name: 'logos/astro'},
             {name: 'logos/typescript'},
             {name: 'logos/tailwind'},
-        ]
+        ],
+        releaseDate: 'APR 2025'
+    },
+    {
+        title: 'Business Dashboard',
+        img: 'img/business-dashboard-1.png',
+        shortDescription: 'Dashboard for visualizing business data. It provides insights and analytics to help make informed decisions.',
+        link: '',
+        githubRepo: 'https://github.com/ezepurro/business-dashboard',
+        techs: [
+            {name: 'logos/react'},
+            {name: 'logos/fastapi'},
+            {name: 'logos/pandas'},
+            {name: 'logos/numpy'},
+            {name: 'logos/docker'},
+        ],
+        releaseDate: 'AUG 2026'
     },
 ];
--- src/data/techData.js
+++ src/data/techData.js
@@ -1,4 +1,3 @@
-
 export const techData = [
     { name: 'logos/javascript', class: 'javascript text-[6rem] text-[#F7DF1E] md:text-white', category: 'Programming Languages', title: 'Javascript' },
     { name: 'logos/typescript', class: 'typescript text-[6rem] text-[#3178C6] md:text-white', category: 'Programming Languages', title: 'Typescript' },
@@ -12,13 +11,18 @@ export const techData = [
     { name: 'logos/tailwind', class: 'tailwind text-[6rem] text-[#38B2AC] md:text-white', category: 'Frontend Development', title: 'TailwindCSS' },
 
     { name: 'logos/django', class: 'django text-[6rem] text-[#092E20] md:text-white', category: 'Backend Development', title: 'Django' },
+    { name: 'logos/fastapi', class: 'fastapi text-[6rem] text-[#009688] md:text-white', category: 'Backend Development', title: 'FastAPI' },
     { name: 'logos/node', class: 'node text-[6rem] text-[#68A063] md:text-white', category: 'Backend Development', title: 'Node.js' },
     { name: 'logos/express', class: 'express text-[6rem] text-white', category: 'Backend Development', title: 'Express.js' },
+
+    { name: 'logos/pandas', class: 'pandas text-[6rem] text-[#150458] md:text-white', category: 'Data Analysis & Libraries', title: 'Pandas' },
+    { name: 'logos/numpy', class: 'numpy text-[6rem] text-[#013243] md:text-white', category: 'Data Analysis & Libraries', title: 'NumPy' },
 
     { name: 'logos/mongodb', class: 'mongodb text-[6rem] text-[#47A248] md:text-white', category: 'Databases', title: 'MongoDB' },
     { name: 'logos/prisma', class: 'prisma text-[6rem] text-[#2D3748] md:text-white', category: 'Databases', title: 'Prisma' },
     { name: 'logos/sqlserver', class: 'sqlserver text-[6rem] text-[#CC2927] md:text-white', category: 'Databases', title: 'SQL Server' },
 
+    { name: 'logos/docker', class: 'docker text-[6rem] text-[#2496ED] md:text-white', category: 'Other tools and technologies', title: 'Docker' },
     { name: 'logos/figma', class: 'figma text-[6rem] text-[#F24E1E] md:text-white', category: 'Other tools and technologies', title: 'Figma' },
     { name: 'logos/git', class: 'git text-[6rem] text-[#F05032] md:text-white', category: 'Other tools and technologies', title: 'Git' },
     { name: 'logos/photoshop', class: 'photoshop text-[6rem] text-[#31A8FF] md:text-white', category: 'Other tools and technologies', title: 'Adobe Photoshop' },
